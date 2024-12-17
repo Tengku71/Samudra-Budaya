ab3271b07a0902254287d3a73067147187c44951
Tengku Dimas <tengkudimasadt@gmail.com>
2024-12-17 15:00:04 +0700

    upload

---
+++ budaya/path.js
@@ -0,0 +1,106 @@
+var path = [
+  {
+    path01: {
+      href: "../aceh/index.html",
+    },
+    path03: {
+      href: "../sumbar/index.html",
+    },
+    path04: {
+      href: "../riau/index.html",
+    },
+    path17: {
+      href: "../bali/index.html",
+    },
+    path19: {
+      href: "../ntt/index.html",
+    },
+    path27: {
+      href: "../sulsel/index.html",
+    },
+    path20: {
+      href: "../kalbar/index.html",
+    },
+    path33: {
+      href: "../papua/index.html",
+    },
+    path02: {
+      href: "../sumut/index.html",
+    },
+    path05: {
+      href: "../jambi/index.html",
+    },
+    path07: {
+      href: "../bengkulu/index.html",
+    },
+    path06: {
+      href: "../sumsel/index.html",
+    },
+    path08: {
+      href: "../lampung/index.html",
+    },
+    path14: {
+      href: "../banten/index.html",
+    },
+    path12: {
+      href: "../jabar/index.html",
+    },
+    path13: {
+      href: "../jateng/index.html",
+    },
+    path15: {
+      href: "../jatim/index.html",
+    },
+    path18: {
+      href: "../ntb/index.html",
+    },
+    path31: {
+      href: "../maluku/index.html",
+    },
+    path34: {
+      href: "../papuabarat/index.html",
+    },
+    path32: {
+      href: "../malukuutara/index.html",
+    },
+    path25: {
+      href: "../sulut/index.html",
+    },
+    path29: {
+      href: "../gorontalo/index.html",
+    },
+    path26: {
+      href: "../sulteng/index.html",
+    },
+    path30: {
+      href: "../sulbar/index.html",
+    },
+    path28: {
+      href: "../sultenggara/index.html",
+    },
+    path10: {
+      href: "../kepriau/index.html",
+    },
+    path09: {
+      href: "../kepbangka/index.html",
+    },
+    path23: {
+      href: "../kaltim/index.html",
+    },
+    path22: {
+      href: "../kalsel/index.html",
+    },
+    path21: {
+      href: "../kalteng/index.html",
+    },
+    path24: {
+      href: "../kalut/index.html",
+    },
+    path11: {
+      href: "../jakarta/index.html",
+    },
+    path16: {
+      href: "../jogja/index.html",
+    },
+  },
+];
